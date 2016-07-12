f54d4aef617a7428acff7b16f853926778975a72
Odigene Joseph <Odigene12@gmail.com>
2016-07-11 20:21:54 -0500

    Added for loop and lines for the cheer

--- Program.cs
+++ Program.cs
@@ -14,12 +14,18 @@ namespace Cheers
             // This is telling the terminal to print this out on the terminal
             Console.Write("> ");
             // The word "String" here tells the program the type you are putting in"
-            String user_name = Console.ReadLine();
+            String user_name = Console.ReadLine(); // Read from terminal save to variable
+
+            // This is a for loop that loops through the user_name because it is a string but also an array
+            for (int i = 0; i < user_name.Length; i++) {
+                Console.WriteLine("Give me a.. " + user_name[i]);
+            }
+
             // The "ToUpper()" method makes the string return all uppercase letters
             // user.name.ToUpper() is turning the user's name to uppercase.
             // ToUpper() is functionality specific to STRINGS
             // ToUpper() accepts no arguments
-            
+
             Console.WriteLine(user_name.ToUpper() + " is..GRAND!");
         }
     }
