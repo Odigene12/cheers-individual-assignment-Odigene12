1be8009fc37038363d4a58145bb3d6d786f81dbc
Odigene Joseph <Odigene12@gmail.com>
2016-07-11 20:28:41 -0500

    Refactored or rearranged cheer prefix and prompt using variables.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,21 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
+            // Variables Section
+            String prompt = "> ";
+            String cheer_prefix = "Give me a";
+
+
             // This will tell the terminal to print the content of the string in the terminal
             Console.WriteLine("What's your name?");
             // This is telling the terminal to print this out on the terminal
-            Console.Write("> ");
+            Console.Write(prompt);
             // The word "String" here tells the program the type you are putting in"
             String user_name = Console.ReadLine(); // Read from terminal save to variable
 
             // This is a for loop that loops through the user_name because it is a string but also an array
             for (int i = 0; i < user_name.Length; i++) {
-                Console.WriteLine("Give me a.. " + user_name[i]);
+                Console.WriteLine(cheer_prefix + "..." + user_name[i]);
             }
 
             // The "ToUpper()" method makes the string return all uppercase letters
