49729f5b879a47243792846d9e3d86a9711d2c18
Odigene Joseph <Odigene12@gmail.com>
2016-07-11 20:07:28 -0500

    Uppercase the username and add comments to code

--- Program.cs
+++ Program.cs
@@ -9,11 +9,18 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
+            // This will tell the terminal to print the content of the string in the terminal
             Console.WriteLine("What's your name?");
-            /// This is telling the terminal to print this out on the terminal
+            // This is telling the terminal to print this out on the terminal
             Console.Write("> ");
-            /// The word "String" here tells the program the type you are putting in"
+            // The word "String" here tells the program the type you are putting in"
             String user_name = Console.ReadLine();
+            // The "ToUpper()" method makes the string return all uppercase letters
+            // user.name.ToUpper() is turning the user's name to uppercase.
+            // ToUpper() is functionality specific to STRINGS
+            // ToUpper() accepts no arguments
+            
+            Console.WriteLine(user_name.ToUpper() + " is..GRAND!");
         }
     }
 }
